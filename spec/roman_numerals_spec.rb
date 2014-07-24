742610630809717b2c4c27077c63bec3c8ba3dba
Linda Gibson & Tanner Steward <student@epicodus.com>
2014-07-24 15:04:34 -0700

    spec and code that limits the number entered to 3999

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -2,7 +2,11 @@ require ('rspec')
 require ('roman_numerals')
 
 describe ('roman_numerals') do
+  it('will not count higher than 3999') do
+    expect(roman_numerals(5000)).to(eq("This is not a roman numeral"))
+  end
+
   it('will take a single digit number and return its roman numeral equivalent') do
-    expect(roman_numerals(1)).to(eq(["I"]))
+    expect(roman_numerals(1)).to(eq("I"))
   end
 end
